8737ffb888f51bc10e8bce6ccd46cde7309e8cc6
Havyas U <113080389+HavyasU@users.noreply.github.com>
2026-02-24 20:17:54 +0530

    Time: 16 ms (29.31%), Space: 46.2 MB (90.8%) - LeetHub

---
+++ 0482-license-key-formatting/0482-license-key-formatting.java
@@ -0,0 +1,24 @@
+class Solution {
+    public String licenseKeyFormatting(String s, int k) {
+        StringBuilder sb = new StringBuilder();
+        int count=0;
+        s = s.toUpperCase();
+        for(int i=s.length()-1;i>=0;i--){
+            char c = s.charAt(i);
+            if(c=='-'){
+                continue;
+            }
+            count++;
+            sb.append(c);
+            if(count==k){
+                sb.append('-');
+                count=0;
+            }
+        }
+        if(sb.length() > 0 && sb.charAt(sb.length()-1)=='-'){
+            sb.deleteCharAt(sb.length()-1);
+        }
+
+        return sb.reverse().toString();
+    }
+}
