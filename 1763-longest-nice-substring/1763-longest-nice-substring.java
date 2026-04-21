b93571b19cfdaa46b26179c6c0f3028a57530bf8
Havyas U <113080389+HavyasU@users.noreply.github.com>
2026-04-21 23:59:21 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 1763-longest-nice-substring/1763-longest-nice-substring.java
+++ 1763-longest-nice-substring/1763-longest-nice-substring.java
@@ -5,22 +5,19 @@ class Solution {
         StringBuilder longest = new StringBuilder();
         for(int i=0;i<n;i++){
             sb.setLength(0);
+            HashSet<Character> hs = new HashSet<>();
             for(int j=i;j<n;j++){
                 sb.append(s.charAt(j));
+                hs.add(s.charAt(j));
                 boolean isNice = true;
                 String currentStr = sb.toString();
                 for(int k=0;k<sb.length();k++){
-                    char toggled;
-                    char cur = sb.charAt(k);
-                    if(cur>='A' && cur<='Z'){
-                        toggled = Character.toLowerCase(cur);
-                    }else{
-                        toggled = Character.toUpperCase(cur);
-                    }
-                    if(!currentStr.contains(toggled+"")){    
-                        isNice = false;
-                        
-                    }
+                    Character lower = Character.toLowerCase(sb.charAt(k));
+                    Character upper = Character.toUpperCase(sb.charAt(k));
+                   if(!hs.contains(lower) || !hs.contains(upper)){
+                    isNice = false;
+                    break;
+                   }
                 }
                 if(isNice){
                     if(sb.length() > longest.length() ){
