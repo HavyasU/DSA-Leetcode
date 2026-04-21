c3b54c7a6fe2e29b3306eb751cc79973494b6b7b
Havyas U <113080389+HavyasU@users.noreply.github.com>
2026-04-22 00:04:36 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 1763-longest-nice-substring/1763-longest-nice-substring.java
+++ 1763-longest-nice-substring/1763-longest-nice-substring.java
@@ -1,27 +1,23 @@
 class Solution {
     public String longestNiceSubstring(String s) {
         int n = s.length();
-        StringBuilder sb = new StringBuilder();
         StringBuilder longest = new StringBuilder();
         for(int i=0;i<n;i++){
-            sb.setLength(0);
             HashSet<Character> hs = new HashSet<>();
             for(int j=i;j<n;j++){
-                sb.append(s.charAt(j));
                 hs.add(s.charAt(j));
                 boolean isNice = true;
-                String currentStr = sb.toString();
-                for(int k=0;k<sb.length();k++){
-                    Character lower = Character.toLowerCase(sb.charAt(k));
-                    Character upper = Character.toUpperCase(sb.charAt(k));
+                for(int k=i;k<=j;k++){
+                    Character lower = Character.toLowerCase(s.charAt(k));
+                    Character upper = Character.toUpperCase(s.charAt(k));
                    if(!hs.contains(lower) || !hs.contains(upper)){
                     isNice = false;
                     break;
                    }
                 }
                 if(isNice){
-                    if(sb.length() > longest.length() ){
-                        longest = new StringBuilder(sb);
+                    if((j - i + 1) > longest.length() ){
+                        longest = new StringBuilder(s.substring(i,j+1));
                     }
                 }
                 
