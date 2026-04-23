ba9655555c5cf42bc3fa0133fa262beaed6d47c1
Havyas U <113080389+HavyasU@users.noreply.github.com>
2026-04-23 23:35:27 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0219-contains-duplicate-ii/0219-contains-duplicate-ii.java
+++ 0219-contains-duplicate-ii/0219-contains-duplicate-ii.java
@@ -3,13 +3,11 @@ class Solution {
         HashMap<Integer,Integer> map = new HashMap<>();
         int n = nums.length;
         for(int i=0;i<n;i++){
-            if(map.containsKey(nums[i])){
-                int ind = map.get(nums[i]);
-                if(Math.abs(i-ind) <= k){
+                int ind = map.getOrDefault(nums[i],-99);
+                if(ind!=-99 && Math.abs(i-ind) <= k){
                     return true;
                 }
-            }
-                map.put(nums[i],i);
+                 map.put(nums[i],i);
             
         }
 
