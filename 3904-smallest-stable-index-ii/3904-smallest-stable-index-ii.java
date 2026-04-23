7a620b61587db008ccd2c45302862273de924999
Havyas U <113080389+HavyasU@users.noreply.github.com>
2026-04-23 17:23:46 +0530

    Time: 11 ms (4.33%), Space: 133.3 MB (16.81%) - LeetHub

---
+++ 3904-smallest-stable-index-ii/3904-smallest-stable-index-ii.java
@@ -0,0 +1,33 @@
+class Solution {
+    public int firstStableIndex(int[] nums, int k) {
+        int max;
+        int n = nums.length;
+        int min[] = new int[n];
+        min[n-1] = nums[n-1];
+    
+        for(int i=n-2;i>=0;i--){
+            if(nums[i]<min[i+1]){
+                min[i] = nums[i];
+            }else{
+                min[i] = min[i+1];
+            }
+        }
+
+        max = nums[0];
+        for(int i=0;i<n;i++){
+            
+            if(nums[i]>max){
+                max = nums[i];
+            }
+            if((max-min[i])<=k){
+                System.out.println(max+" "+min[i]);
+                return i;
+            }
+           
+        }
+
+        return -1;
+
+
+    }
+}
