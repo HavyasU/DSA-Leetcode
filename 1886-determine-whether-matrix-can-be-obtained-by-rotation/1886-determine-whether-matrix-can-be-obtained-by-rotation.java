d2fb25bcc9cdfcb3baf14f01ad82cc8cdcc77023
Havyas U <113080389+HavyasU@users.noreply.github.com>
2026-05-04 18:33:13 +0530

    Time: 12 ms (0.94%), Space: 44 MB (7.33%) - LeetHub

---
+++ 1886-determine-whether-matrix-can-be-obtained-by-rotation/1886-determine-whether-matrix-can-be-obtained-by-rotation.java
@@ -0,0 +1,41 @@
+class Solution {
+    public boolean rotateMat(int[][] mat, int[][] target){
+        int n  = mat.length;
+        for(int i=0;i<n;i++){
+            for(int j=0;j<n;j++){
+                if(j>i){
+                    int ele = mat[i][j];
+                    mat[i][j] = mat[j][i];
+                    mat[j][i]=ele;
+                }
+            }
+        }
+
+        for(int i=0;i<n;i++){
+            for(int j=0;j<n/2;j++){
+                    int ele = mat[i][j];
+                    mat[i][j] = mat[i][n-j-1];
+                    mat[i][n-j-1] =ele;
+            }
+        }
+
+
+        for(int i=0;i<n;i++){
+            for(int j=0;j<n;j++){
+                System.out.println(mat[i][j]);
+                if(mat[i][j]!=target[i][j]){
+                    return false;
+                }
+            }
+        }
+        return true;
+    }
+    public boolean findRotation(int[][] mat, int[][] target) {
+        for(int i=0;i<4;i++){
+            if(rotateMat(mat,target)){
+                return true;
+            }
+        }
+        return false;
+    }
+}
