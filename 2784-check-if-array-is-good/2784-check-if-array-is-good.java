4002d0ea2cfb88597678ecc5520d1ea050b22f4a
Havyas U <113080389+HavyasU@users.noreply.github.com>
2026-05-14 11:52:31 +0530

    Time: 5 ms (48.38%), Space: 44.9 MB (52%) - LeetHub

--- 2784-check-if-array-is-good/2784-check-if-array-is-good.java
+++ 2784-check-if-array-is-good/2784-check-if-array-is-good.java
@@ -4,11 +4,6 @@ class Solution {
        int n = nums[nums.length-1];
        if(nums.length<=1 || nums[nums.length-2]!=n) return false;
        for(int i=0;i<nums.length-1;i++){
-        if(nums.length-2 == i){
-            if(nums[i]!=n){
-                return false;
-            }
-        }
         if(nums[i] != i+1){
             return false;
         }
