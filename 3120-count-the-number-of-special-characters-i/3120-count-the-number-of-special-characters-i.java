fbddad9f4f79cebb31cddb041e496eea1b2dba4a
Havyas U <113080389+HavyasU@users.noreply.github.com>
2026-05-28 11:29:21 +0530

    Time: 1 ms (100%), Space: 43.5 MB (78.14%) - LeetHub

--- 3120-count-the-number-of-special-characters-i/3120-count-the-number-of-special-characters-i.java
+++ 3120-count-the-number-of-special-characters-i/3120-count-the-number-of-special-characters-i.java
@@ -1,19 +1,16 @@
 class Solution {
     public int numberOfSpecialChars(String word) {
-        int letters[] = new int[52];
-        int count = 0;
-        for(char c:word.toCharArray()){
-            if(Character.isLowerCase(c))
-                letters[c-'a']++;
-            else
-                letters[c-'A'+26]++;
+        int count = 0 ;
+        int[] freq = new int[58];
+        for(int i = 0 ; i<word.length(); i++){
+            char ch = word.charAt(i);
+            freq[ch-'A']++;
         }
-
-        for(int i=0;i<letters.length/2;i++){
-            if(letters[i]>0 && letters[i+26]>0){
+        for(int i = 0 ; i<58; i++){
+             if ((i >= 0 && i <= 25) && freq[i] > 0 &&freq[i + 32] > 0){
                 count++;
             }
         }
-        return count;
+        return count ;
     }
 }
