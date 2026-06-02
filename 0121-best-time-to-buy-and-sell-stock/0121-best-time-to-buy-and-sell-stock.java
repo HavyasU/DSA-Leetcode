49da44c8020296a71b3c3d72f7cef69da7d88ea9
Havyas U <113080389+HavyasU@users.noreply.github.com>
2026-06-02 23:38:35 +0530

    Time: 2 ms (40.06%), Space: 89 MB (98.34%) - LeetHub

--- 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.java
+++ 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.java
@@ -5,7 +5,7 @@ class Solution {
         int max = -999;
         int profit=0;
         for(int i=n-1;i>=0;i--){
-            bigs[i] = max; 
+            bigs[i] = max;
             if(max<prices[i]){
                 max = prices[i];
             }
