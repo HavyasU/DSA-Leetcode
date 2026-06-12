3770a5e4d93e17aed1edb6370b6005d8dceaea48
Havyas U <113080389+HavyasU@users.noreply.github.com>
2026-06-12 15:28:42 +0530

    Time: 3 ms (4.19%), Space: 43.3 MB (5.82%) - LeetHub

--- 0500-keyboard-row/0500-keyboard-row.java
+++ 0500-keyboard-row/0500-keyboard-row.java
@@ -4,19 +4,32 @@ class Solution {
         String firstRow = "qwertyuiop";
         String secondRow = "asdfghjkl";
         String thirdRow = "zxcvbnm";
-        for(String word : words){
-            boolean valid1 = true;
-            boolean valid2 = true;
-            boolean valid3 = true;
-            for(char c : word.toLowerCase().toCharArray()){
-                if(!firstRow.contains(c+"")) valid1=false;
-                if(!secondRow.contains(c+"")) valid2=false;
-                if(!thirdRow.contains(c+"")) valid3=false;
+
+        String merged = String.join(":",words)+":";
+        System.out.println(merged);
+        int latestPoint = 0;
+        int valid1 = 0;
+        int valid2 = 0;
+        int valid3 = 0;
+        for(int i=0;i<merged.length();i++){
+            char c = merged.toLowerCase().charAt(i);
+            if(c==':'){
+                int len = i-latestPoint;
+                if(valid1 == len || valid2==len || valid3==len){
+                    lst.add(merged.substring(latestPoint,i));
+                }
+                latestPoint = i+1; 
+                valid1=0;
+                valid2=0;
+                valid3=0;
+            }else{
+            if(firstRow.contains(c+"")) valid1++;
+            if(secondRow.contains(c+"")) valid2++;
+            if(thirdRow.contains(c+"")) valid3++;
             }
-            if(valid1  ||  valid2 || valid3){
-            lst.add(word);
-            }
+           
         }
+       
         
 
         return lst.toArray(new String[0]);
