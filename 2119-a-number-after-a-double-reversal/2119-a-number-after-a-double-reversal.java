f6081474e609f01b0a998c8e0bc0b5c5e059b503
Havyas U <113080389+HavyasU@users.noreply.github.com>
2026-06-16 09:14:49 +0530

    Time: 0 ms (100%), Space: 42.4 MB (11.41%) - LeetHub

--- 2119-a-number-after-a-double-reversal/2119-a-number-after-a-double-reversal.java
+++ 2119-a-number-after-a-double-reversal/2119-a-number-after-a-double-reversal.java
@@ -1,18 +1,5 @@
 class Solution {
     public boolean isSameAfterReversals(int num) {
-        boolean containsZero = false;
-        while(num!=0){
-        int d = num%10;
-        if(d==0){
-            containsZero = true;
-        }else if(containsZero){
-            return false;
-        }else{
-            return true;
-        }
-        num=num/10;
-      }  
-
-    return true;
+        return num==0 || (num%10)!=0;
     }
 }
