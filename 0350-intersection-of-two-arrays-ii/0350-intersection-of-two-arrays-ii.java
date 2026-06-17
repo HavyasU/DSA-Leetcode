5f39361d8f480802735762aea46d3e86f851ee0a
Havyas U <113080389+HavyasU@users.noreply.github.com>
2026-06-17 14:22:49 +0530

    Time: 0 ms (100%), Space: 44.7 MB (86%) - LeetHub

--- 0350-intersection-of-two-arrays-ii/0350-intersection-of-two-arrays-ii.java
+++ 0350-intersection-of-two-arrays-ii/0350-intersection-of-two-arrays-ii.java
@@ -1,33 +1,19 @@
 class Solution {
     public int[] intersect(int[] nums1, int[] nums2) {
-        Arrays.sort(nums1);
-        Arrays.sort(nums2);
-
-        int n1 = nums1.length;
-        int n2 = nums2.length;
-
-        int point1=0;
-        int point2=0;
-
-        ArrayList<Integer> lst = new ArrayList<>();
-
-        while(point1<n1 && point2<n2){
-            if(nums1[point1] == nums2[point2]){
-                lst.add(nums1[point1]);
-                point1++;
-                point2++;
-            }else if(nums1[point1]<nums2[point2]){
-                point1++;
-            }else{
-                point2++;
-            }
-        }
-
-        int res[] = new int[lst.size()];
+        int freq[] = new int[1001];
+        int res[] = new int[Math.min(nums1.length,nums2.length)];
         int idx = 0;
-        for(int ele : lst){
-            res[idx++] = ele;
+        for(int num: nums1){
+            freq[num]++;
         }
-        return res;
+
+        for(int num:nums2){
+            if(freq[num]> 0){
+                res[idx++] = num;
+            }
+            freq[num]--;
+        }
+
+        return Arrays.copyOfRange(res,0,idx);
     }
 }
