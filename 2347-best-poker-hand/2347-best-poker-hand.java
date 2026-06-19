7854c202ddf862a3652520b5bf4763dfb252955f
Havyas U <113080389+HavyasU@users.noreply.github.com>
2026-06-19 17:52:38 +0530

    Time: 3 ms (7.42%), Space: 43 MB (32.75%) - LeetHub

---
+++ 2347-best-poker-hand/2347-best-poker-hand.java
@@ -0,0 +1,38 @@
+class Solution {
+    public String bestHand(int[] ranks, char[] suits) {
+        int samelen = 0;
+        Arrays.sort(suits);
+        Arrays.sort(ranks);
+        int count = 1;
+        int max=0;
+        for(int i=1;i<suits.length;i++){
+            if(suits[i] == suits[i-1])
+            {
+                count++;
+            }else{
+                count = 1;
+            }
+            max = Math.max(max,count);
+        }
+
+        if(max == 5) return "Flush";
+        max = 0;
+        count = 1;
+        for(int i=1;i<ranks.length;i++){
+            if(ranks[i] == ranks[i-1])
+            {
+                count++;
+            }else{
+                count = 1;
+            }
+            max = Math.max(max,count);
+        }
+            System.out.println(max);
+        if(max >=3) return "Three of a Kind";
+        else if(max==2) return "Pair";
+
+
+
+        return "High Card";
+    }
+}
