1dd6193d3cefd302728b47dd24deb4ff93ea51cb
Havyas U <113080389+HavyasU@users.noreply.github.com>
2026-06-20 10:23:31 +0530

    Time: 2 ms (98.08%), Space: 43.5 MB (15.42%) - LeetHub

---
+++ 0020-valid-parentheses/0020-valid-parentheses.java
@@ -0,0 +1,34 @@
+class Solution {
+    public char reverse(char c){
+        if(c==')'){
+            return '(';
+        }
+        if(c==']'){
+            return '[';
+        }
+        if(c=='}'){
+            return '{';
+        }
+        return '\0';
+    }
+    public boolean isValid(String s) {
+        Stack<Character> stk = new Stack<>();
+
+        for(char c : s.toCharArray()){
+            if(c=='(' || c=='['  || c=='{'){
+                stk.add(c);
+            }else{
+                if(stk.isEmpty()) return false;
+                if(stk.pop() != reverse(c)){
+                    return false;
+                }
+            }
+        }
+
+        if(stk.isEmpty()){
+            return true;
+        }else{
+            return false;
+        }
+    }
+}
