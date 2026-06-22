62fc3afb030621792171c24da262681178797f22
Havyas U <113080389+HavyasU@users.noreply.github.com>
2026-06-22 21:50:02 +0530

    Time: 1 ms (99.95%), Space: 77.2 MB (65.11%) - LeetHub

--- 0053-maximum-subarray/0053-maximum-subarray.java
+++ 0053-maximum-subarray/0053-maximum-subarray.java
@@ -3,11 +3,12 @@ class Solution {
         int sum  = 0;
         int maxsum = Integer.MIN_VALUE;
         for(int ele : nums){
+            if(sum < 0){
+                sum=ele;
+            }else
             sum+=ele;
             if(sum>maxsum) maxsum = sum; 
-            if(sum < 0){
-                sum=0;
-            }
+           
         }
 
         return maxsum;
