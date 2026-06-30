2335aa559aa95e6437d46d70ff583809a48921af
Havyas U <113080389+HavyasU@users.noreply.github.com>
2026-06-30 23:44:57 +0530

    Time: 1 ms (100%), Space: 43.5 MB (94.54%) - LeetHub

--- 0008-string-to-integer-atoi/0008-string-to-integer-atoi.java
+++ 0008-string-to-integer-atoi/0008-string-to-integer-atoi.java
@@ -1,25 +1,29 @@
 class Solution {
     public int myAtoi(String s) {
-        s = s.trim();
-        if(s.length() == 0) return 0;
-        long res = 0;
-        boolean isNeg = false;
-        isNeg = s.charAt(0) == '-';
-        int i = (isNeg || s.charAt(0) == '+' )? 1:0;
-        for(;i<s.length();i++){
-            char c = s.charAt(i);
-            if(c>='0' && c<='9'){
-                res = res*10 + (int)(c-'0');
-            }
-            else break;
-            if(isNeg && -res <= Integer.MIN_VALUE) return Integer.MIN_VALUE;
-            if(!isNeg && res >= Integer.MAX_VALUE) return Integer.MAX_VALUE;
+        s =s.trim();
+        boolean neg = false;
+        int start =0;
+        int n = s.length();
+        if(n==0) return 0;
+        if(s.charAt(0)=='-') {
+            neg = true;
+            start = 1;
+        }else if(s.charAt(0) == '+'){
+            start = 1;
         }
-        
-        if(isNeg){
-            return (int)-res;
+        long rev= 0;
+        while(start<n){
+            char c = s.charAt(start);
+            if(Character.isDigit(c)){
+            rev=(rev*10)+(c-'0');
+            }else break;
+            if(neg && -rev <= Integer.MIN_VALUE) return Integer.MIN_VALUE;
+            if(!neg && rev >= Integer.MAX_VALUE) return Integer.MAX_VALUE;
+            start++;
         }
 
-        return (int)res;
+
+        if(neg) return(int) -rev;
+        return (int)rev;
     }
 }
