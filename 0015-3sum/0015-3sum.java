acffd5f258fc62188f792686b86ae1a3c17106f8
Havyas U <113080389+HavyasU@users.noreply.github.com>
2026-07-03 15:28:37 +0530

    Time: 366 ms (13.87%), Space: 60.6 MB (5.82%) - LeetHub

--- 0015-3sum/0015-3sum.java
+++ 0015-3sum/0015-3sum.java
@@ -1,34 +1,31 @@
 class Solution {
     public List<List<Integer>> threeSum(int[] nums) {
-        Set<List<Integer>> res = new HashSet<>();
-        Arrays.sort(nums);
+        HashSet<List<Integer>> lst = new HashSet<>();
         int n = nums.length;
-        for(int i=0;i<n;i++){
-            int ele = nums[i];
-            int left = i+1;
-            int right = n-1;
-            int target = 0 - (ele);
-
-            int sum = 0;
-            while(left<right){
-                sum=nums[left]+nums[right];
-                if((sum == target)){
-                    if(i<left && left < right && i < right){
-                    List<Integer> lst = new ArrayList<>();
-                    lst.add(ele);
-                    lst.add(nums[left]);
-                    lst.add(nums[right]);
-                    res.add(lst);
-                    }
-                    left++;
-                    right--;
-                }else if(sum>target){
-                    right--;
-                }else {
-                    left++;
+        Arrays.sort(nums);
+        for (int i = 0; i < n; i++) {
+            int cur = nums[i];
+            int start = i + 1;
+            int end = n - 1;
+            List<Integer> sub = new ArrayList<>();
+            while (start < end) {
+                int sum = nums[start] + nums[end] + cur;
+                if (sum == 0) {
+                    sub.add(cur);
+                    sub.add(nums[start]);
+                    sub.add(nums[end]);
+                    lst.add(sub);
+                    sub = new ArrayList<>();
+                    start++;
+                    end--;
+                } else if (sum < 0) {
+                    start++;
+                } else {
+                    end--;
                 }
             }
+
         }
-        return new ArrayList(res);
+        return new ArrayList<>(lst);
     }
 }
