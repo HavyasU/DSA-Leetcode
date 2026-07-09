9b78875e899ed11566c233c192258bd6cb250235
Havyas U <113080389+HavyasU@users.noreply.github.com>
2026-07-09 13:05:08 +0530

    Time: 0 ms (100%), Space: 41.9 MB (86.48%) - LeetHub

--- 1025-divisor-game/1025-divisor-game.java
+++ 1025-divisor-game/1025-divisor-game.java
@@ -1,20 +1,5 @@
 class Solution {
     public boolean divisorGame(int n) {
-        boolean isAlice = true;
-        int x;
-        while(true){
-            for(x=1;x<n/2;x++){
-                if(n%x == 0){
-                    break;
-                }
-            }
-
-            if(x>(n/2)){
-                return !isAlice;
-            }
-
-            n = n-x;
-            isAlice = !isAlice;
-        }
+        return n%2 == 0;
     }
 }
