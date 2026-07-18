de8deb6288e474d2dc1f1b315f38e2ee6736807c
Havyas U <113080389+HavyasU@users.noreply.github.com>
2026-07-18 22:48:01 +0530

    Time: 1 ms (100%), Space: 44.1 MB (31.47%) - LeetHub

--- 3498-reverse-degree-of-a-string/3498-reverse-degree-of-a-string.java
+++ 3498-reverse-degree-of-a-string/3498-reverse-degree-of-a-string.java
@@ -3,7 +3,7 @@ class Solution {
         int sum = 0;
         int n = s.length();
         for(int i=0;i<n;i++){
-            sum+= ((26) - (int)(s.charAt(i)-'a'))*(i+1);
+            sum+= ((int)('z'-'a'+1) - (int)(s.charAt(i)-'a'))*(i+1);
         }
         return sum;
     }
