dc2b807db56bf0201387c4a6ab9f2d5cd6189fe2
Havyas U <113080389+HavyasU@users.noreply.github.com>
2026-07-19 23:42:14 +0530

    Time: 1 ms (76.35%), Space: 44 MB (58.33%) - LeetHub

--- 3996-even-number-of-knight-moves/3996-even-number-of-knight-moves.java
+++ 3996-even-number-of-knight-moves/3996-even-number-of-knight-moves.java
@@ -1,8 +1,5 @@
 class Solution {
     public boolean canReach(int[] start, int[] target) {
-        int startsum = start[0]+start[1];
-        int targetsum = target[0]+target[1];
-
-        return startsum%2 == targetsum%2;
+        return (start[0]+start[1])%2 == (target[0]+target[1])%2;
     }
 }
