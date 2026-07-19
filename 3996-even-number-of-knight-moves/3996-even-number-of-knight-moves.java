08617d1a3b4911819a2ff4aac5ad3b3fecdf5646
Havyas U <113080389+HavyasU@users.noreply.github.com>
2026-07-19 23:41:20 +0530

    Time: 1 ms (76.35%), Space: 43.8 MB (97.72%) - LeetHub

--- 3996-even-number-of-knight-moves/3996-even-number-of-knight-moves.java
+++ 3996-even-number-of-knight-moves/3996-even-number-of-knight-moves.java
@@ -3,7 +3,6 @@ class Solution {
         int startsum = start[0]+start[1];
         int targetsum = target[0]+target[1];
 
-        if(startsum%2 == targetsum%2) return true;
-        else return false;
+        return startsum%2 == targetsum%2;
     }
 }
